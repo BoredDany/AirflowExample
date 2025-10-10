c2cabd809df9b04edccea709055d70387be1d675
BoredDany <martinezq.d@javeriana.edu.co>
2025-10-10 14:35:30 -0500

    feat(completed user process)

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# AirflowExample
-This repository contains an example of using airflow to do a simple process: Check if an API is available, extract some data from it, and save it in a database
+# Airflow example
+
+This repository contains an example of using Airflow to do the following process:
+
+* Create a table in a PostgreSQL database
+* Check if an API is available (https://jsonplaceholder.typicode.com/users)
+* Obtain user data from the API
+* Save the user data in the PostgreeSQL database
+
+## Requirements
+
+- [Docker Desktop](https://www.docker.com/products/docker-desktop/) (choose based on your OS):
+  - [Windows](https://docs.docker.com/desktop/install/windows-install/)
+  - [macOS (Intel/Apple Silicon)](https://docs.docker.com/desktop/install/mac-install/)
+  - [Linux (Docker Engine)](https://docs.docker.com/engine/install/)
+
+- [Docker Engine](https://docs.docker.com/engine/) (for Linux-based systems)
+
+- [UV (optional)](https://docs.astral.sh/uv/getting-started/installation/#__tabbed_1_1)
+
+
+
+## Steps to run the project
+
+
+### Create virtual environment
+
+´´´bash
+uv venv --python 3.11
+´´´
+
+
+### Activate virtual environmen
+
+#### macOS/Linux:
+´´´bash
+source .venv/bin/activate
+´´´
+
+#### Windows (PowerShell):
+´´´bash
+.venv\Scripts\Activate.ps1
+´´´
+
+
+### Install Apache Airflow
+
+´´´bash
+uv pip install apache-airflow==3.0.0
+´´´
+
+
+### Run the project
+
+´´´bash
+docker compose up
+´´´
+
+After that you'll be able to access Airflow in your browser [Here](localhost:8080/), and you'll see a login screen, so to access the main Airflow dashboard type the default credentials:
+
+* user: airflow
+* password: airflow
+
+You´ll have to add the postgre connection through the Airflow web UI.
+
+
+## Providers for Airflow
+
+You can find providers to install [Here](https://registry.astronomer.io/providers) if you need to interact with other services, and set the connections in Airflow. For this example, you need Redshift SQL Hook provider, for installing it, run the following command
+
+´´´bash
+uv pip install apache-airflow-providers-postgres==6.1.3
+´´´
+
+
+## Test a task
+
+To test a task without running an entire DAG and saving the run metadata, go to the scheduler container and execute:
+
+´´´bash
+bin/bash
+airwflow tasks test <task_name> <task_id>
+´´´
---
+++ dags/user_processing.py
@@ -0,0 +1,89 @@
+from airflow.sdk import dag, task
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.sdk.bases.sensor import PokeReturnValue
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+
+@dag
+def user_processing():
+    
+    create_table = SQLExecuteQueryOperator(
+        task_id="create_table",
+        conn_id="postgres",
+        sql="""
+        CREATE TABLE IF NOT EXISTS users (
+            id INT PRIMARY KEY,
+            firstname VARCHAR(255),
+            lastname VARCHAR(255),
+            email VARCHAR(255),
+            city VARCHAR(255),
+            phone VARCHAR(50),
+            website VARCHAR(255),
+            company VARCHAR(255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        """
+    )
+
+    # Check condition every 30 seconds, and after 300 seconds the sensor fails if the condition is still false
+    @task.sensor(poke_interval=30, timeout=300)
+    def is_api_available():
+        import requests
+        response = requests.get("https://jsonplaceholder.typicode.com/users")
+        print(response.status_code)
+
+        if response.status_code == 200:
+            condition = True
+            fake_users = response.json()
+        else:
+            condition = False
+            fake_users = None
+
+        return PokeReturnValue(is_done=condition, xcom_value=fake_users)
+
+    @task
+    def extract_user(fake_users):
+        
+        if not fake_users:
+            raise ValueError("No users available from API")
+    
+        first = fake_users[0]
+        firstname, *rest = first.get("name", "").split()
+        lastname = " ".join(rest) if rest else ""
+        user_row = {
+            "id": first.get("id"),
+            "firstname": firstname,
+            "lastname": lastname,
+            "email": first.get("email"),
+            "city": first.get("address", {}).get("city"),
+            "phone": first.get("phone"),
+            "website": first.get("website"),
+            "company": first.get("company", {}).get("name"),
+        }
+        print(user_row)
+        return user_row
+    
+    @task
+    def save_user_csv(user_info):
+        import csv
+        from datetime import datetime
+        
+        user_info["created_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        
+        with open("/tmp/user_info.csv", "w", newline="") as file:
+            fieldnames = ["id", "firstname", "lastname", "email", "city", "phone", "website", "company", "created_at"]
+            writer = csv.DictWriter(file, fieldnames=fieldnames, quoting=csv.QUOTE_MINIMAL)
+            writer.writeheader()
+            writer.writerow(user_info)
+            
+    @task
+    def store_user():
+        hook = PostgresHook(postgres_conn_id="postgres")
+        hook.copy_expert(
+            sql="COPY users (id, firstname, lastname, email, city, phone, website, company, created_at) FROM STDIN WITH CSV HEADER",
+            filename="/tmp/user_info.csv"
+        )
+
+    save_user_csv(extract_user(create_table >> is_api_available())) >> store_user()
+    
+user_processing()
